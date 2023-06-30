2804adf88fe359081fbcd8a4d8f1e8887eb0b488
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-06-30 04:14:54 +0100

    changed prod db to postgresql

--- website/config/config.py
+++ website/config/config.py
@@ -4,14 +4,14 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-db_name = 'locale'
+db_name = 'localedb'
 
 
-# default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', db_name)
+default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', db_name)
 
-# uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
-# if uri.startswith('postgres://'):
-#     uri = uri.replace('postgres://', 'postgresql://', 1)
+uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
 
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
@@ -33,7 +33,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://' # An SQL memory database is used
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = config('DEBUG', False, cast=bool)
 
